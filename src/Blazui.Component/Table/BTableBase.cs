06411b5cb8f1effb66c47bb65cf4cc8042eab032
张哲 <zhang.zhe@synyi.com>
2019-11-10 22:37:16 +0800

    update

---
+++ src/Blazui.Component/Table/BTableBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.Table
+{
+    public class BTableBase<TRow> : ComponentBase
+    {
+        [Parameter]
+        public IReadOnlyCollection<TableHeader> Headers { get; set; }
+
+        [Parameter]
+        public IReadOnlyCollection<TRow> Rows { get; set; }
+
+        protected override void OnInitialized()
+        {
+        }
+    }
+}
---
+++ src/Blazui.Component/Table/TableHeader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.Table
+{
+    public class TableHeader
+    {
+        public int Width { get; set; }
+        public string Text { get; set; }
+    }
+}
